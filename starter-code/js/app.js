48c0eb425faa1f608c9b267799b84000fc06a8eb
Andrew <andrew.vinocur@gmail.com>
2016-09-15 08:47:49 -0700

    adds hangman angular work

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,4 +1,15 @@
 console.log('app.js loaded!');
 
 // initialize the application
-var app = angular.module("hangmanApp", []);
+
+  var app = angular.module("hangmanApp", []);
+  app.controller('hangmanCtrl', hangmanCtrl);
+
+  function hangmanCtrl () {
+    var vm = this;
+    vm.game = new HangmanGame('elephant');
+    vm.letterGuess = function() {
+      vm.game.guess(vm.userInput);
+      vm.userInput= "";
+    }
+  }
